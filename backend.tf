29307013f380ccfdf107fd5bf7f972b8d9514c00
Sandy Colmenares <sm4rc3cd@gmail.com>
2026-07-27 13:48:50 -0400

    Replace storage account name in backend.tf
    Updated storage account name placeholder in backend configuration.

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG-TerraformState"
-    storage_account_name = "saaumlabsmarcecd"
+    storage_account_name = "REPLACE_WITH_YOUR_STORAGE_ACCOUNT_NAME"
     container_name       = "tfstate"
     key                  = "aum-lab.tfstate"
     # Separate from ntfs-lab.terraform.tfstate and rbac-lab.terraform.tfstate
